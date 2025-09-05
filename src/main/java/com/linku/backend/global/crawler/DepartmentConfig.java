89a09cfafccc7774cdacfc8e112a4d99c6eb12ea
Shinjongyun <tigerjong123@gmail.com>
2025-09-05 23:46:13 +0900

    #10 [feat]: DepartmentConfig

---
+++ src/main/java/com/linku/backend/global/crawler/DepartmentConfig.java
@@ -0,0 +1,8 @@
+package com.linku.backend.global.crawler;
+
+public class DepartmentConfig {
+    private String name;
+    private String url;
+    private boolean isRss;
+    private String contentSelector; // HTML 파서용
+}
