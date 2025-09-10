e0222598040cd04c15d2e33121421d6c4982a83c
Shinjongyun <tigerjong123@gmail.com>
2025-09-11 02:11:26 +0900

    #10 [feat]: departmentConfigId에 속하는 알림들 조회

--- src/main/java/com/linku/backend/domain/alert/repository/AlertRepository.java
+++ src/main/java/com/linku/backend/domain/alert/repository/AlertRepository.java
@@ -3,6 +3,9 @@ package com.linku.backend.domain.alert.repository;
 import com.linku.backend.domain.alert.Alert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AlertRepository extends JpaRepository<Alert, Long> {
     boolean existsByUrl(String url);
+    List<Alert> findByDepartmentConfigIdIn(List<Long> departmentConfigIds);
 }
