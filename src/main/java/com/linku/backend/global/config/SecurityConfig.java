bcf33731cba2dc774b003d621329a9a5fb0aa9ca
kisusu <kikisu115@naver.com>
2025-11-23 16:17:14 +0900

    hotfix: SecurityConfig 설정 복구

--- src/main/java/com/linku/backend/global/config/SecurityConfig.java
+++ src/main/java/com/linku/backend/global/config/SecurityConfig.java
@@ -1,15 +1,24 @@
 package com.linku.backend.global.config;
 
-import jakarta.servlet.http.HttpServletRequest;
+import com.linku.backend.domain.oauth.OAuthFailureHandler;
+import com.linku.backend.domain.oauth.OAuthService;
+import com.linku.backend.domain.oauth.OAuthSuccessHandler;
+import com.linku.backend.global.jwt.JwtAuthenticationFilter;
+import com.linku.backend.global.jwt.JwtExceptionFilter;
+import com.linku.backend.global.jwt.JwtTokenService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -18,62 +27,61 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenService jwtTokenService;
 
     @Value("${spring.front.origin}")
     private String frontOrigin;
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public DefaultOAuth2UserService defaultOAuth2UserService() {
-        return new DefaultOAuth2UserService();
-    }
+    public SecurityFilterChain filterChain(
+            HttpSecurity httpSecurity,
+            OAuthService oAuthService,
+            OAuthSuccessHandler oAuthSuccessHandler,
+            OAuthFailureHandler oAuthFailureHandler) throws Exception {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
 
-        //CORS 설정
         httpSecurity
-                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-
-                        CorsConfiguration configuration = new CorsConfiguration();
-
-                        configuration.setAllowedOrigins(List.of(frontOrigin));
-                        configuration.setAllowedHeaders(List.of("*"));
-                        configuration.setAllowedMethods(List.of("*"));
-                        configuration.setAllowCredentials(true);
-                        configuration.setMaxAge(3600L);
-
-                        configuration.setExposedHeaders(Arrays.asList("Authorization", "Set-Cookie"));
-
-                        return configuration;
-                    }
-                }));
-
-        //CSRF(Cross-Site Request Forgery) 보호 기능 비활성화
-        httpSecurity
-                .csrf((auth) -> auth.disable());
-
-        //Spring Security의 기본 폼 로그인 방식(Username & Password 로그인 폼) 비활성화
-        httpSecurity
-                .formLogin((auth) -> auth.disable());
-
-        //HTTP Basic 인증 방식 비활성화
-        httpSecurity
-                .httpBasic((auth) -> auth.disable());
+                .cors(corsCustomizer ->
+                        corsCustomizer.configurationSource(corsConfigurationSource()))          //CORS 설정
+                .csrf(AbstractHttpConfigurer::disable)                  //CSRF(Cross-Site Request Forgery) 보호 기능 비활성화
+                .formLogin(AbstractHttpConfigurer::disable)             //Spring Security의 기본 폼 로그인 방식(Username & Password 로그인 폼) 비활성화
+                .httpBasic(AbstractHttpConfigurer::disable);            //HTTP Basic 인증 방식 비활성화
 
         //경로별 인가 작업
+        HttpSecurity roleGuest = httpSecurity
+                .oauth2Login((oauth) ->
+                        oauth.userInfoEndpoint(userInfoEndpointConfig ->
+                                        userInfoEndpointConfig.userService(oAuthService))
+                                .successHandler(oAuthSuccessHandler)
+                                .failureHandler(oAuthFailureHandler))
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/auth/send-code",
+                                "/auth/verify-code"
+                        ).hasAuthority("ROLE_GUEST")
+                        .requestMatchers(
+                                "/api/example/to-be-authenticated"
+                        ).authenticated()
+                        .anyRequest().permitAll()
+                )
+
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenService), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JwtExceptionFilter(), JwtAuthenticationFilter.class);
+
+
+        //세션을 사용하지 않는 방식(STATELESS)으로 설정
         httpSecurity
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/api/example/to-be-authenticated").authenticated()
-                        .anyRequest().permitAll())
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .exceptionHandling(customizer -> customizer
                         .authenticationEntryPoint((request, response, authException) -> {
@@ -82,12 +90,34 @@ public class SecurityConfig {
                         .accessDeniedHandler((request, response, accessDeniedException) -> {
                             response.setStatus(403);
                         }));
-
-        //세션을 사용하지 않는 방식(STATELESS)으로 설정
-        httpSecurity
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
         return httpSecurity.build();
     }
+
+
+    @Bean
+    public RoleHierarchyImpl roleHierarchy() {
+        return RoleHierarchyImpl.fromHierarchy("""
+                ROLE_MEMBER > ROLE_GUEST
+                """);
+    }
+
+
+    @Bean
+    public DefaultOAuth2UserService defaultOAuth2UserService() {
+        return new DefaultOAuth2UserService();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(frontOrigin));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Set-Cookie"));
+
+        return request -> configuration;
+    }
+
 }
