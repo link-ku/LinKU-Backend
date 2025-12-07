533c626bffce1ae4b3d9604106a19b17d2cb0dba
Jihwan Kim <kjhwan0802@naver.com>
2025-12-08 02:34:45 +0900

    hotfix: add CorsUtils package

--- src/main/java/com/linku/backend/global/config/SecurityConfig.java
+++ src/main/java/com/linku/backend/global/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
 
 import java.util.Arrays;
 import java.util.List;
