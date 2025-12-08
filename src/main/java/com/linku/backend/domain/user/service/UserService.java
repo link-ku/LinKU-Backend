58e1717790063eaaeee327a7530dadb4771d1661
lyouxsun <leedrkr323@konkuk.ac.kr>
2025-12-08 10:23:06 +0900

    fix: 인증 후 토큰 발급

--- src/main/java/com/linku/backend/domain/user/service/UserService.java
+++ src/main/java/com/linku/backend/domain/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.linku.backend.domain.user.service;
 import com.linku.backend.domain.oauth.dto.GoogleUserInfo;
 import com.linku.backend.domain.user.User;
 import com.linku.backend.domain.user.repository.UserRepository;
+import com.linku.backend.global.auth.dto.AuthTokenResponse;
 import com.linku.backend.global.auth.dto.UserInfoResponse;
 import com.linku.backend.global.exception.LinkuException;
 import com.linku.backend.global.jwt.JwtTokenService;
@@ -39,6 +40,7 @@ public class UserService {
         Long userId = jwtTokenService.extractUserIdByGuestToken(guestToken);
         User user = getUserById(userId);
         user.updateInfo(kuMail);
-        return UserInfoResponse.from(user);
+        AuthTokenResponse response = jwtTokenService.generateAuthToken(user);
+        return UserInfoResponse.from(user, response);
     }
 }
