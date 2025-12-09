4c9ee922842eb464f974e7354e681c36dafb6a9a
Shinjongyun <162339483+Shinjongyun@users.noreply.github.com>
2025-12-09 16:58:55 +0900

    [Feat] 나의 구독한 학과 리스트 업 정렬 기능 (#31)
    * [Feat] #30 구독학과 오름차순 정렬 기능
    
    * [Refact] #30 테스트 코드 리팩토링
    
    * [Refact] #30 테스트 코드 리팩토링
    
    * [Fix] #30 Resolver에서 authenticaiton이 null일 경우 에러

--- src/main/java/com/linku/backend/domain/subscribe/repository/SubscribeRepository.java
+++ src/main/java/com/linku/backend/domain/subscribe/repository/SubscribeRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
-    List<Subscribe> findByUser_UserId(Long userId);
+    List<Subscribe> findByUser_UserIdOrderByDepartmentConfig_IdAsc(Long userId);
     void deleteByUser_UserIdAndDepartmentConfig_Id(Long userId, Long departmentConfigId);
     boolean existsByUser_UserIdAndDepartmentConfig_Id(Long userId, Long configId);
 }
--- src/main/java/com/linku/backend/global/resolver/CurrentUserIdArgumentResolver.java
+++ src/main/java/com/linku/backend/global/resolver/CurrentUserIdArgumentResolver.java
@@ -1,13 +1,18 @@
 package com.linku.backend.global.resolver;
 
 import com.linku.backend.global.auth.AuthUser;
+import com.linku.backend.global.exception.LinkuException;
 import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import com.linku.backend.global.response.ResponseCode;
+
+import java.nio.file.attribute.UserPrincipal;
 
 @Component
 public class CurrentUserIdArgumentResolver implements HandlerMethodArgumentResolver {
@@ -23,7 +28,13 @@ public class CurrentUserIdArgumentResolver implements HandlerMethodArgumentResol
                                   NativeWebRequest webRequest,
                                   WebDataBinderFactory binderFactory) throws Exception {
 
-        AuthUser principal = (AuthUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw LinkuException.of(ResponseCode.GOOGLE_INVALID_USER_INFO_RESPONSE);
+        }
+
+        AuthUser principal = (AuthUser) authentication.getPrincipal();
         return principal.getId();
     }
 }
